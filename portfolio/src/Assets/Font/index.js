7221282b08c6befeef8bb67f13a97aa00b8ee95a
yongjunhan <hanyongjun.dev@gmail.com>
2023-01-18 19:45:31 +0900

    스크롤 이벤트

---
+++ portfolio/src/Assets/Font/index.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  @font-face {
+    font-family: 'Pretendard';
+    src: url("../Assets/Font/PretendardVariable.woff2");
+  }
+`
--- portfolio/src/Components/Projects/Pro/Traveloper.js
+++ portfolio/src/Components/Projects/Pro/Traveloper.js
@@ -1,17 +1,30 @@
-import React,{useEffect, useState} from 'react'
+import React,{useRef, useEffect, useState} from 'react'
 import axios from 'axios'
 import { Navigate, useNavigate, useNavigation } from 'react-router-dom';
 import styled from 'styled-components'
+import './Traveloper.css'
 
 import {IoArrowBackOutline} from 'react-icons/io5' 
 import {RiHome6Line} from 'react-icons/ri'
 
 function Traveloper() {
-
+  
   const navigate = useNavigate();
+  const [position, setPosition] = useState(0);
+  
+  useEffect(() => {
+    function onScroll() {
+      setPosition(window.scrollY);
+    }
+    window.addEventListener("scroll", onScroll);
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+  }, []);  
 
   return (
     <BG> 
+      {/* 헤더 */}
       <Header>
         <BackContainer 
           onClick={()=>{
@@ -21,7 +34,7 @@ function Traveloper() {
             color='white'
             size={24}/>
         </BackContainer>
-
+        <Title>댕트립</Title>
         <HomeContainer>
           <RiHome6Line
           size='24'
@@ -31,9 +44,20 @@ function Traveloper() {
           }}
           />
         </HomeContainer> 
-
       </Header>
-      <div></div>
+      {/* 메인영역 */}
+      <MainArea
+        onScroll={()=>{
+          console.log(position)
+        }}
+      >
+        <div className='cover'>
+          <div></div>
+          <div></div>
+        </div>
+
+        <div className='overview'></div>
+      </MainArea>
     </BG>
   )
 }
@@ -41,18 +65,17 @@ function Traveloper() {
 const BG  = styled.div`
   width: 100vw;
   height : 100vh;
-  background-color : #1e1e1e;
   display : flex;
   flex-direction : column;
 `
 
 const Header = styled.div`
   width : 100%;
-  height : 100px;
-  border : 1px solid red;
+  height : 80px;
   display : flex;
   justify-content : space-between;
   padding : 0 140px;
+  background-color : #1e1e1e;
   align-items :center;
 `
 const BackContainer = styled.div`
@@ -69,4 +92,22 @@ const HomeContainer = styled.div`
   justify-content : flex-end;
   align-items : center;
 `
+const Title = styled.div`
+  font-family : Pretendard;
+  font-size : 1.2rem;
+  color : white;
+  text-align : center;
+`
+const MainArea = styled.div`
+  height : 100%;
+  width : 100%;
+  overflow: scroll;
+`
+
+const Cover = styled.div`
+  width: 100%;
+  height : 1000px;
+  border : 3px solid salmon;
+`
+
 export default Traveloper
